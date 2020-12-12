30f457373905d4319e373f817ad8a528aa5f7a3b
Mosh Feu <moshfeu@gmail.com>
2020-12-12 21:43:58 +0200

    initial

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `MosheF Dev blog`,
+    author: `Mosh Feuchtwanger`,
+    description: ``,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/moshfeu`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/moshfeu`,
+      },
+    ],
+  },
+}
